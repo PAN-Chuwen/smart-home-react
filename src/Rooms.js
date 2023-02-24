fe429c507b58dadeb8060251c9b992ef8276856f
PAN-Chuwen <panchuwen2000@gmail.com>
2023-02-24 11:52:37 +0800

    Update UI on rooms

--- src/Rooms.js
+++ src/Rooms.js
@@ -5,26 +5,34 @@ export default function Rooms() {
   return (
     <div className="rooms">
       <div className="roomBox">
-        <h2 className="titleBox">Living Room</h2>
-        <h3>4 devices</h3>
+        <div className="roomInfo">
+          <h1>Living Room</h1>
+          <h3>4 devices</h3>
+        </div>
         <img src="/images/icons8-living-room-100.png" alt="living-room" />
       </div>
 
       <div className="roomBox">
-        <h2>kitchen</h2>
-        <h3>3 devices</h3>
+        <div className="roomInfo">
+          <h1>Kitchen</h1>
+          <h3>3 devices</h3>
+        </div>
         <img src="/images/icons8-kitchen-room-100.png" alt="kitchen" />
       </div>
 
       <div className="roomBox">
-        <h2>fitting Room</h2>
-        <h3>4 devices</h3>
+        <div className="roomInfo">
+          <h1>Fitting room</h1>
+          <h3>2 devices</h3>
+        </div>
         <img src="/images/icons8-fitting-room-100.png" alt="fitting-room" />
       </div>
 
-      <div className="office">
-        <h2>office</h2>
-        <h3>4 devices</h3>
+      <div className="roomBox">
+        <div className="roomInfo">
+          <h1>Office</h1>
+          <h3>4 devices</h3>
+        </div>
         <img src="/images/icons8-office-100.png" alt="office" />
       </div>
     </div>
